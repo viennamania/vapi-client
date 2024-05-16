1728166c1284666e3d291d29b0d8f7c50c44a6be
seunghyonpark <creath.park@gmail.com>
2024-05-16 12:07:20 +0900

    gpt-4o

--- assistants/assistant.ts
+++ assistants/assistant.ts
@@ -5,7 +5,10 @@ export const assistant: CreateAssistantDTO | any = {
   name: "Paula-broadway",
   model: {
     provider: "openai",
-    model: "gpt-3.5-turbo",
+    
+    //model: "gpt-3.5-turbo",
+    model: "gpt-4o",
+
     temperature: 0.7,
     
     ///systemPrompt: `You're Paula, an AI assistant who can help the user decide what do he/she wants to watch on Broadway. User can ask you to suggest shows and book tickets. You can get the list of available shows from broadway and show them to the user, and then you can help user decide which ones to choose and which broadway theatre they can visit. After this confirm the details and book the tickets. `,
