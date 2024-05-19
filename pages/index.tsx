532f18efc598b301c01876c3a477ce9f6e246c8b
seunghyonpark <creath.park@gmail.com>
2024-05-19 12:17:02 +0900

    good helth morning

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,13 +10,31 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-12 ${inter.className}`}
     >
       <div className="text-center">
-        <h1 className="text-3xl">Welcome to Broadway Show Assistant</h1>
-        <p className="text-slate-600">
+        <h1 className="text-3xl">Welcome to AI Companion</h1>
+
+        <p className="text-slate-600 mt-2 text-lg">
+          Your personal assistant to help you with your daily tasks.
+        </p>
+
+        <p className="text-slate-600 mt-2 text-lg">
           {/*
           Talk with Sarah to explore upcoming shows and book tickets.
           */}
-          Talk with Sarah.
+          Talk with KyungHee.
         </p>
+
+        {/* powered by famnote */}
+        <div className="mt-4">
+          <a
+            href="#"
+            target="_blank"
+            className="text-sm text-slate-500"
+          >
+            Powered by Famnote
+          </a>
+        </div>
+
+
       </div>
       <Assistant />
     </main>
